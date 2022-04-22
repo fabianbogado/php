d464a53f456758875aa52bbf44a3abdbfe2df457
fabianbogado <fabicrack218@gmail.com>
2022-04-22 20:18:16 -0300

    cambios

---
+++ formulario_login/acceso_confirmado.php
@@ -0,0 +1,36 @@
+<?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="conteiner">
+        <div class="row">
+            <div class="col-12">
+                <h1>Bienvenid@ al sistema</h1>
+            </div>
+            <div class="col-12" style="background-color: #098 ;" >
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel rem iure, officia animi optio esse necessitatibus, sunt, natus facilis doloremque molestiae blanditiis eveniet laboriosam quos distinctio earum adipisci magni sed!</p>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary">salir</button>
+                </div>
+            </div>
+
+        </div>
+
+    
+    
+    </div>
+</body>
+</html>
