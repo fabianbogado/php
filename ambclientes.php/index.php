6ce363bab128dbd359c26b9af0b212f88feb1333
fabianbogado <fabicrack218@gmail.com>
2022-05-10 21:02:59 -0300

    cambios

---
+++ ambclientes.php/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro de clientes</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1>Registro de clientes</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <form method="POST" action="">
+                    <div class="pb-3">
+                        <label for="txtDni">Dni*</label>
+                        <input type="text" name="txtDni" id="txtDni" class="form-control">
+                    </div>
+                    <div class="pb-3">
+                        <label for="txtNombre">Nombre*</label>
+                        <input type="text" name="txtNombre" id="txtNombre" class="form-control">
+                    </div>
+                    <div class="pb-3">
+                        <label for="txtTelefono">Telefono*</label>
+                        <input type="text" name="txtTelefono" id="txtTelefono" class="form-control">
+                    </div>
+                    <div class="pb-3">
+                        <label for="txtCorreo">Correo*</label>
+                        <input type="email" name="txtCorreo" id="txtCorreo" class="form-control">
+                    </div>
+                    <div>
+
+                    </div>
+                    <div class="pb-3">
+                        <p>Archivos admitidos: .jpg, pneg ,.png</p>
+                    </div>
+                    <div class="pb-3">
+                        <button type="submit" class="btn btn-primary">Guardar</button>
+                        <button type="submit" class="btn btn-danger">Nuevo</button>
+                    </div>
+                </form>
+                <div class="col-12">
+                    <table class="table table-border">
+                        
+
+                    </table>
+
+
+                </div>
+             </div>
+        </div>
+    </main>
+
+</body>
+
+</html>
